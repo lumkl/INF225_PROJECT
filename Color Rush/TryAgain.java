8f2e8da6ba04c5063f9c5d725aaa7d5314037c3e
Keith Niño P. Lumelay <120693819+lumkl@users.noreply.github.com>
2023-03-21 21:37:10 +0800

    Create TryAgain.java

---
+++ Color Rush/TryAgain.java
@@ -0,0 +1,58 @@
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TryAgain extends JFrame {
+    TryAgain() {
+        super("Candy Rush");
+        setLayout(new FlowLayout());
+
+        JLabel label = new JLabel();
+        label.setText("GAME OVER!");
+        label.setFont(new Font(("Times New Roman"), Font.CENTER_BASELINE, 50));
+
+        JButton tryAgainButton = new JButton();
+        tryAgainButton.setText("BETTER LUCK NEXT TIME");
+        tryAgainButton.setFont(new Font(("Times New Roman"), Font.CENTER_BASELINE, 20));
+
+        TryAgainEventHandler tryHandler = new TryAgainEventHandler();
+        tryAgainButton.addActionListener(tryHandler);
+
+        JButton exitButton = new JButton();
+        exitButton.setText("EXIT");
+        exitButton.setFont(new Font(("Times New Roman"), Font.CENTER_BASELINE, 20));
+
+        ExitEventHandler exitHandler = new ExitEventHandler();
+        exitButton.addActionListener(exitHandler);
+
+
+        add(label);
+        add(tryAgainButton);
+        add(exitButton);
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.setSize(500, 500);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.getContentPane().setBackground(Color.LIGHT_GRAY);
+    }
+        private class TryAgainEventHandler implements ActionListener {
+
+            public void actionPerformed(ActionEvent event) {
+                App myApp = new App();
+                myApp.main(null);
+                dispose();
+    }
+}
+private class ExitEventHandler implements ActionListener {
+
+    public void actionPerformed(ActionEvent event) {
+        dispose();
+    }
+}
+}
